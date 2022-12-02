7ecbe38140215899cef9ef1d49f9726442ace8d8
river0g <yui.0g.0418@gmail.com>
2022-12-03 01:08:38 +0900

    rpc枠実装

--- src/routes/lesson1.py
+++ src/routes/lesson1.py
@@ -13,12 +13,6 @@ description: rpc(json-rpc)と他のwebapiについて理解する。
 router = APIRouter()
 
 
-class JsonRPCBody(BaseModel):
-    jsonrpc: str
-    method: str
-    params: Optional[List[str]]
-
-
 class Body(BaseModel):
     method: str
     params: Optional[str]  # e.g) param1,param2
@@ -59,3 +53,45 @@ async def rpc(data: Body):
         return {"msg": str(e)}
 
     return {"msg": "success", "res": res}
+
+
+class JsonRPCBody(BaseModel):
+    jsonrpc: str
+    method: str
+    # params: Optional[List[str]]
+    id: str
+
+
+class JsonRPCBody1(BaseModel):
+    jsonrpc: str
+    method: str
+    params: Optional[List[str]]
+    id: str
+
+
+@method  # jsonrpc
+def add_10(*params) -> Result:
+    curr = 0
+    for param in params:
+        curr += int(param)
+
+    return Success(curr)
+
+
+@router.post("/lesson1/jsonrpc")
+async def jsonrpc(request: Request, data: JsonRPCBody1):
+    return Response(dispatch(await request.body()))
+
+
+@method
+def ping() -> Result:
+    return Success("pong")
+
+
+@router.post("/lesson1/jsonrpc1")
+async def index(request: Request, data: JsonRPCBody):
+    return Response(dispatch(await request.body()))
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=5000)
